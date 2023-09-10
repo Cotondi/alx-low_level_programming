ef8d133a918cb654a53c848ff866acb7ef73ce4f
Cotondi <nundaclinton@gmail.com>
2023-09-10 03:21:50 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  *main - function of the task
+  *Return: always zero
+  */
+
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+purchar(i);
+
+for (i = 'a'; i <= 'z'; i++
+putchar(i);
+putchar('\n');
+
+return (0);
+}
