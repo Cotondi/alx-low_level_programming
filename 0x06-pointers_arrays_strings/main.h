56fc16c8443c57f634aaefd52ab7b1c641583444
Cotondi <nundaclinton@gmail.com>
2023-09-20 14:16:15 -0700

    Write a function that concatenates two strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c)
+#include <stdio.h>
+#include <string.h>
+
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -14,4 +17,5 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *leet(char *);
+
 #endif
