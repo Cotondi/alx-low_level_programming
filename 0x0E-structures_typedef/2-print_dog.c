8674d24895a7208d5e0576b505b6fcac937a329c
Cotondi <nundaclinton@gmail.com>
2023-10-10 15:12:09 -0700

    %s/^[[:space:]]\+//g

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "dog.h" /* Make sure dog.h defines the struct dog */
+#include "dog.h"
 
 /**
-* print_dog - Display details of a dog structure
-* @dog_instance: Pointer to the dog structure to print
+* print_dog - Display the details of a dog structure.
+* @d: Pointer to the dog structure to print.
 */
-void print_dog(struct dog *dog_instance)
+void print_dog(struct dog *d)
 {
-if (dog_instance == NULL)
+if (d == NULL)
+{
 return;
+}
 
-if (dog_instance->name == NULL)
-dog_instance->name = "(Name not provided)";
-if (dog_instance->owner == NULL)
-dog_instance->owner = "(Owner not specified)";
+if (d->name == NULL)
+{
+d->name = "(nil)";
+}
+if (d->owner == NULL)
+{
+d->owner = "(nil)";
+}
 
-printf("Dog's Name: %s\nAge: %f\nOwner: %s\n",
-dog_instance->name, dog_instance->age, dog_instance->owner);
+printf("Name: %s\nAge: %f\nOwner: %s\n",
+d->name, d->age, d->owner);
 }
