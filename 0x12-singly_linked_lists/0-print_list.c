c8fe9e12d01c543d77c117a78350efc1b6ff3314
Cotondi <nundaclinton@gmail.com>
2023-10-19 15:08:13 -0700

    a function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,54 +1,25 @@
+#include <stdio.h>
 #include "lists.h"
-int _putchar(char c);
+
 /**
-* print_list - Output the elements of a list_t list.
-* @h: A pointer to the head of the list.
-*
-* Return: The number of nodes in the list.
+* print_list - Outputs the elements of a list_t list.
+* @h: A reference to the starting point of the list.
+* Return: The number of nodes.
 */
 size_t print_list(const list_t *h)
 {
-size_t node_tally = 0;
-unsigned int len;
-const char *str;
+size_t tally = 0;
 
-for (; h != NULL; h = h->next)
+while (h != NULL)
 {
 if (h->str == NULL)
-{
-node_tally++;
-_putchar('[');
-_putchar('0');
-_putchar(']');
-_putchar(' ');
-_putchar('(');
-_putchar('n');
-_putchar('i');
-_putchar('l');
-_putchar(')');
-_putchar('\n');
-}
+printf("[0] (nil)\n");
 else
-{
-node_tally++;
-_putchar('[');
-len = h->len;
-while (len > 0)
-{
-_putchar(len % 10 + '0');
-len /= 10;
-}
-_putchar(']');
-_putchar(' ');
-str = h->str;
-while (*str)
-{
-_putchar(*str);
-str++;
-}
-_putchar('\n');
-}
+printf("[%d] %s\n", h->len, h->str);
+
+tally++;
+h = h->next;
 }
 
-return (node_tally);
+return (tally);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,12 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stddef.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 /**
 * struct list_s - singly linked list
 * @str: string - (malloc'ed string)
