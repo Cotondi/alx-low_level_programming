4430d09b050fffe2b93c993be678b5bfdae99a13
Cotondi <nundaclinton@gmail.com>
2023-10-20 18:41:44 -0700

    a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,19 @@
 section .data
-hello db "Hello, Holberton,",10,0
-format db "%s", 0
+; Define data section
+hello db "Hello, Holberton,", 10, 0 ; Message with newline and null-terminator
+format db "%s", 0                ; Format string for printf
 
 section .text
+; Define text section
 global main
 extern printf
 
 main:
+; Main function
 push rbp
-mov rdi, format
-mov rsi, hello
-call printf
+mov rdi, format         ; Set format string
+mov rsi, hello          ; Set message
+call printf             ; Call printf
 pop rbp
-ret
+ret                     ; Return
+
