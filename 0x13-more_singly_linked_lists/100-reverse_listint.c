f06a457932e9e3913e2f7ca8fa8e5b00d0474e0a
Cotondi <nundaclinton@gmail.com>
2023-10-24 14:21:39 -0700

    Write a function that reverses a listint_t linked list.

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -3,7 +3,7 @@
 /**
 * reverse_listint - Reverses a listint_t linked list.
 * @head: A pointer to a pointer to the head of the list.
-* Returns: A pointer to the first node of the reversed list.
+* Return: A pointer to the first node of the reversed list.
 */
 listint_t *reverse_listint(listint_t **head)
 {
