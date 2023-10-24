75237a24ad05e63d3888c18b799763fe84a89eee
Cotondi <nundaclinton@gmail.com>
2023-10-24 10:02:25 -0700

    a function that frees a listint_t list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,17 +13,21 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 listint_t *new_node;
 listint_t *current;
 new_node = malloc(sizeof(listint_t));
-if (new_node == NULL) 
+if (new_node == NULL)
 {
 return (NULL);
 }
 new_node->n = n;
 new_node->next = NULL;
-if (*head == NULL) {
+if (*head == NULL)
+{
 *head = new_node;
-} else {
+}
+else
+{
 current = *head;
-while (current->next != NULL) {
+while (current->next != NULL)
+{
 current = current->next;
 }
 current->next = new_node;
