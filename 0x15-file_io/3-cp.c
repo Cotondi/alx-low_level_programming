61472f4ed04c29aea914d8fb42884de85eeb049d
Cotondi <nundaclinton@gmail.com>
2023-10-30 13:02:40 -0700

    Write a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,79 +1,70 @@
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 
-#define BUFFER_SIZE 1024
-
 /**
-* _putchar - Writes a character to stdout.
-* @c: The character to write.
-*
-* Return: On success, the character written.
-*         On error, -1 is returned.
+* error_exit - Handles errors and terminates program with specified exit code.
+* @code: Error code.
+* @msg: Prints the error message.
+* @arg: Specifies the argument for the error message.
 */
-int _putchar(char c)
+void error_exit(int code, char *msg, char *arg)
 {
-return (write(STDOUT_FILENO, &c, 1));
-}
-
-/**
-* error_exit - Prints an error message and exits with the specified code.
-* @code: The exit code.
-* @msg: The error message.
-* @arg: The additional argument for the error message.
-*/
-void error_exit(int code, const char *msg, const char *arg)
-{
-const char *ptr;
-
-ptr = arg;
-
-while (*ptr)
-_putchar(*ptr++);
-
-write(STDERR_FILENO, msg, sizeof(msg) - 1);
+dprintf(STDERR_FILENO, msg, arg);
 exit(code);
 }
 
 /**
-* main - Copies the content of a file to another file.
-* @argc: The number of arguments.
-* @argv: The arguments.
-*
-* Return: 0 on success, or the corresponding exit code on failure.
+* copy_file - Duplicates the contents of a file to another file.
+* @file_from: The source file.
+* @file_to: The destination file.
 */
-int main(int argc, char **argv)
+void copy_file(char *file_from, char *file_to)
 {
-int fd_from, fd_to, bytes_read, bytes_written;
+int fd_from, fd_to;
+ssize_t bytes_read, bytes_written;
 char buffer[BUFFER_SIZE];
 
-if (argc != 3)
-error_exit(97, "Usage: cp file_from file_to\n", "");
-
-fd_from = open(argv[1], O_RDONLY);
+fd_from = open(file_from, O_RDONLY);
 if (fd_from == -1)
-error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+error_exit(98, "Error: Can't read from file %s\n", file_from);
 
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (fd_to == -1)
-error_exit(99, "Error: Can't write to %s\n", argv[2]);
+error_exit(99, "Error: Can't write to %s\n", file_to);
 
 while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 {
 bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written == -1)
-error_exit(99, "Error: Can't write to %s\n", argv[2]);
+if (bytes_written != bytes_read)
+error_exit(99, "Error: Can't write to %s\n", file_to);
 }
 
 if (bytes_read == -1)
-error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+error_exit(98, "Error: Can't read from file %s\n", file_from);
 
 if (close(fd_from) == -1)
-error_exit(100, "Error: Can't close fd ", argv[1]);
+error_exit(100, "Error: Can't close fd %d\n", (char *)(intptr_t)fd_from);
 
 if (close(fd_to) == -1)
-error_exit(100, "Error: Can't close fd ", argv[2]);
+error_exit(100, "Error: Can't close fd %d\n", (char *)(intptr_t)fd_to);
+}
+
+/**
+* main - Entry point, copies the content of one file to another.
+* @ac: Number of arguments.
+* @av: Array of arguments.
+*
+* Return: Always 0.
+*/
+int main(int ac, char **av)
+{
+if (ac != 3)
+error_exit(97, "Usage: cp file_from file_to\n", "");
+
+copy_file(av[1], av[2]);
 
 return (0);
 }
